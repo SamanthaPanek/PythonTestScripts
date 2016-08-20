58bd168f056c021aad8cc32e2f115951e1ce3927
SamanthaPanek <sampanek@gmail.com>
2016-08-19 22:08:06 -0400

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# PythonTestScripts
-# PythonTestScripts
---
+++ README.py
@@ -0,0 +1,15 @@
+# PythonTestScripts
+# Bio
+
+print Bio()
+
+# creates a function that prints out my biography
+def Bio():
+  print ""
+  print "Hi my name is Samantha Panek and I'm a Junior "
+  print "Software Verification Engineer at Cree in "
+  print "Durham, NC. I write test automation scripts "
+  print "for our IOT PoE Intellegent Lighing Solution."
+  print "This page stores some of the examples of my "
+  print "work."
+  print ""
