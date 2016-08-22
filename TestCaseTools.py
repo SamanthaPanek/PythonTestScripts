0a2a7f04d9b1f312ce3f82f58928b0a96d4a5381
SamanthaPanek <sampanek@gmail.com>
2016-08-22 10:30:16 -0400

    Create TestCaseTools.py

---
+++ TestCaseTools.py
@@ -0,0 +1,22 @@
+# TestCaseTools Library
+# Creating a library that can be imported to run test cases
+# Samantha Panek
+# August 22, 2016
+#------------------------------------------------------------------------------
+
+import pyautogui
+
+# creates a function to be called checking whether an image is
+# present on the user's screen
+def imageDetection(computerUsername):
+    image= "/Users/" + computerUsername + "/Desktop/Capture.PNG"
+    coordinates= pyautogui.locateCenterOnScreen(image)
+    if (coordinates is None):
+        print ("Failed. Image not detected on screen.")
+    else:
+        print ("Pass. Image detected on screen.")
+
+# asks the user for their computer user name to be called in other programs
+def inputUsername():
+    computerUsername= input("Enter your computer username as a string: ")
+    return computerUsername
