f3779dcda65ad8f2c037ac80ab5f85f8bdaf14e9
SamanthaPanek <sampanek@gmail.com>
2017-05-08 16:58:44 -0400

    Update README.py

--- README.py
+++ README.py
@@ -6,12 +6,11 @@
 # creates a function that prints out my biography
 def Bio():
   print ""
-  print "Hi my name is Samantha Panek and I'm a Junior "
-  print "Software Verification Engineer at Cree in "
-  print "Durham, NC. I write test automation scripts "
-  print "for our IOT PoE Intellegent Lighing Solution."
+  print "Hi my name is Samantha Panek and I'm a "
+  print "QA Automation Engineer at Smashing Boxes in "
+  print "Durham, NC."
   print "This page stores some of the examples of my "
-  print "work."
+  print "past work."
   print ""
 
 # prints the function Bio to the console  
